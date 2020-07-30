75963ab271f19a19c551fc7e7c1732f7b8aa42f5
Rolf Madsen <{ID}+{username}@users.noreply.github.com>
2020-07-30 14:00:57 +0200

    Minor change

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ const Home = () => (
   <Layout>
     <div className="block relative border-solid border-2 border-gray-600 clear-both py-2 px-2 mx-5 my-5">
       <h1 className="block text-2xl">Welcome!</h1>
-      <p className="block">This site contains experiments and early prototypes.</p>
+      <p className="block">This site contains experimental and early prototypes.</p>
     </div>
   </Layout>
 )
