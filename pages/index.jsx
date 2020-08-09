59c6e352c319fa7ad093ccce0f9b3377b6ad3f42
rolfmadsen <rolfmadsen@users.noreply.github.com>
2020-08-09 13:38:50 +0200

    Fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,7 @@ import SearchResult from '../components/SearchResult';
 
 const Home = () => (
   <Layout>
-<<<<<<< HEAD
     <SearchResult />
-=======
-    <div className="block relative border-solid border-2 border-gray-600 clear-both py-2 px-2 mx-5 my-5">
-      <h1 className="block text-2xl">Welcome!</h1>
-      <p className="block">This site contains experimental and early prototypes.</p>
-    </div>
->>>>>>> 75963ab271f19a19c551fc7e7c1732f7b8aa42f5
   </Layout>
 )
 
