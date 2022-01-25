78013d21e38e7f0e94db7c7a3d4f7c3747f84ce4
rolfmadsen <96595+rolfmadsen@users.noreply.github.com>
2022-01-25 15:19:39 +0100

    Updates and minor changes to configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 // tailwind.config.js
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', 
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
