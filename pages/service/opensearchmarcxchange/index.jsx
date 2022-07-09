553102d573c22b68df0f319906c9d57642db8e92
rolfmadsen <96595+rolfmadsen@users.noreply.github.com>
2022-07-09 17:11:12 +0200

    Added information that the Opensearch API (marcXchange) page is identical to the frontpage.

--- pages/service/opensearchmarcxchange/index.jsx
+++ pages/service/opensearchmarcxchange/index.jsx
@@ -4,6 +4,9 @@ import SearchResultDkabm from '../../../components/OpensearchMarcxchange';
 
 const Home = () => (
   <Layout>
+    <div className="block relative border-solid border-2 border-gray-600 clear-both py-2 px-2 mx-4 my-4">
+      <p className="block">This page is identical to the frontpage.</p>
+    </div>
     <SearchResultDkabm />
   </Layout>
 )
