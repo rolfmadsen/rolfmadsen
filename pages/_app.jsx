7ed12d0bf2d2558f3848df7d5a989c157d0729d0
rolfmadsen <96595+rolfmadsen@users.noreply.github.com>
2023-12-11 20:54:42 +0100

    Bugfixes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,10 @@
 // pages/_app.jsx
 import '../styles/globals.css'
-import PiwikProProvider from '@piwikpro/next-piwik-pro'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <PiwikProProvider
-        containerId={process.env.NEXT_PUBLIC_CONTAINER_ID}
-        containerUrl={process.env.NEXT_PUBLIC_CONTAINER_URL} 
-      >
         <Component {...pageProps} />
-      </PiwikProProvider>
     </>
   )
 }
