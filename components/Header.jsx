626f2548a3af38044a601e025985c1b2a5cb8cdf
Rolf Madsen <rolfmadsen@pop-os.localdomain>
2024-09-29 16:04:29 +0200

    Improvement to mobile styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,23 +5,25 @@ import { useRouter } from 'next/router';
 export default function Header() {
   const router = useRouter();
   return (
-    <header className="clear-both w-full bg-gray-800 text-white py-4 px-6">
-      <h1 className="text-4xl">
-        <Link href="/">
-          <button
-            className="text-teal-500 font-normal"
-            title="Til forsiden"
-            onClick={(e) => {
-              if (router.pathname !== "/") {
-                e.preventDefault();
-                router.push("/");
-              }
-            }}
-          >
-            Metadata & Discovery
-          </button>
-        </Link>
-      </h1>
+    <header className="w-full bg-gray-800 text-white py-4">
+      <div className="max-w-full mx-auto px-4">
+        <h1 className="text-4xl">
+          <Link href="/">
+            <button
+              className="text-teal-500 font-normal"
+              title="Til forsiden"
+              onClick={(e) => {
+                if (router.pathname !== "/") {
+                  e.preventDefault();
+                  router.push("/");
+                }
+              }}
+            >
+              Metadata & Discovery
+            </button>
+          </Link>
+        </h1>
+      </div>
     </header>
   );
 }
